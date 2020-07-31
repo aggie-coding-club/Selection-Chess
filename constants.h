03fb0e636b6e6ddf42c099447a5a3d3572b21ac9
jose01carrillo30 <34354911+jose01carrillo30@users.noreply.github.com>
2020-07-30 23:23:07 -0500

    fixed small convention

--- constants.h
+++ constants.h
@@ -10,12 +10,12 @@ typedef unsigned char PieceEnum;
 */
 enum : PieceEnum {EMPTY=0, W_PAWN, B_PAWN, W_ROOK, B_ROOK, W_KNIGHT, B_KNIGHT, W_BISHOP, B_BISHOP, W_QUEEN, B_QUEEN, W_KING, B_KING};
 
-static const PieceEnum blackPieces[6] = {B_PAWN, B_ROOK, B_KNIGHT, B_BISHOP, B_QUEEN, B_KING};
-static const PieceEnum whitePieces[6] = {W_PAWN, W_ROOK, W_KNIGHT, W_BISHOP, W_QUEEN, W_KING};
+static const PieceEnum BLACK_PIECES[6] = {B_PAWN, B_ROOK, B_KNIGHT, B_BISHOP, B_QUEEN, B_KING};
+static const PieceEnum WHITE_PIECES[6] = {W_PAWN, W_ROOK, W_KNIGHT, W_BISHOP, W_QUEEN, W_KING};
 
 // piece value list:
-// used as follows: PIECEVALUES[W_PAWN], for example
-const short PIECEVALUES[] = {0, 100, -100, 500, -500, 300, -300, 300, -300, 900, -900, 10000, -10000};
+// used as follows: PIECE_VALUES[W_PAWN], for example
+const short PIECE_VALUES[] = {0, 100, -100, 500, -500, 300, -300, 300, -300, 900, -900, 10000, -10000};
 
 static const char* EMPTY_BOARD_FEN = "8/8/8/8/8/8/8/8 w - - 0 1";
 
